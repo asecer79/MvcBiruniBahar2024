6e02ad6ca5eb3edd593ad1eb9363f442c8a7142c
asecer79 <asecer79@DESKTOP-TPOST9I>
2024-03-13 14:29:40 +0300

    y

---
+++ W03/Models/SchoolDb.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+
+namespace W03.Models
+{
+    public static class SchoolDb
+    {
+        public static List<Student>? Students =new List<Student>();
+        
+        static bool _isDbInitialized = false;
+        public static void InitializeDb(int n)
+        {
+            if (!_isDbInitialized)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    var student = new Faker<Student>()
+                        .RuleFor(s => s.Id, r => i)
+                        .RuleFor(s => s.Name, r => r.Name.FullName())
+                        .RuleFor(s => s.Email, (r, s) => r.Internet.Email(s.Name))
+                        .RuleFor(s => s.Age, r => r.Random.Int(17, 30));
+
+                    Students.Add(student);
+
+
+                }
+
+                _isDbInitialized = true;
+            }
+
+        }
+
+
+    }
+}
---
+++ W03/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace W03.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name cannot be blank!")]
+        public string? Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Bad Email format!")]
+        public string? Email { get; set; }
+
+        [Range(17,30,ErrorMessage = "Age must be between 17 and 30")]
+        public int? Age { get; set; }
+
+    }
+}
